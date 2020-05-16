3f4954b25c0ee752b15263a5a1880c44eef84c88
corinnaj <corinna.jaschek@gmail.com>
2020-05-16 12:30:23 +0200

    delete now duplicate speech package

--- Assets/Voice/Scripts/Speech Output Core Scripts/SpeechBase.cs
+++
@@ -1,8 +0,0 @@
-﻿
-public abstract class SpeechBase
-{
-    public static bool isSpeaking = false;
-    public abstract void Init();
-    public abstract void Speak(string text);
-    public abstract void Stop();
-}
